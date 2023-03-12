46929cdd93c125d4531f5f1b1678566af25268d5
haze998 <docenko6996@gmail.com>
2023-03-12 15:14:29 +0200

    add fonts

--- SpaceX/SpaceX/ViewController.swift
+++ SpaceX/SpaceX/ViewController.swift
@@ -5,15 +5,45 @@
 //  Created by Evgeniy Docenko on 08.03.2023.
 //
 
+import SnapKit
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
+    private lazy var lable: UILabel = {
+        let label = UILabel()
+        label.text = "Canvas on UIKit"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 30)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupConstraints()
+    }
+    
+    private func setupUI() {
+        
+    }
+    
+    private func setupConstraints() {
+        view.addSubview(lable)
+        lable.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
 }
 
+// MARK: - Canvas Preview
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ViewController().showPreview()
+    }
+}
